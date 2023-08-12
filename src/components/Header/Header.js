a12290443c5e98dcbc41fb6e3558578af67347d4
Dimitri Williams <dwilliams_64@yahoo.com>
2023-08-12 15:33:05 -0500

    Feat: Created basic responsive layout for header and navigation.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">#VANLIFE</Link>
-      <nav>
+    <header className="d-flex justify-content-between">
+      <nav className="d-sm-none fs-1">&#9776;</nav>
+      <Link className="fs-1" to="/">
+        #VANLIFE
+      </Link>
+      <nav className="d-none d-sm-flex d-md-flex">
         <Link to="/about">About</Link>
         <Link>Vans</Link>
       </nav>
