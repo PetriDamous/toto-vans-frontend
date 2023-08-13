fe13471b04d06d612e59c4233178517af8d9de3b
Dimitri Williams <dwilliams_64@yahoo.com>
2023-08-13 14:37:17 -0500

    Refact: Changed div element to nav element in HostLayout to have better symantic html.

--- src/components/HostLayout/HostLayout.js
+++ src/components/HostLayout/HostLayout.js
@@ -4,12 +4,12 @@ import { Link, Outlet } from "react-router-dom";
 const HostLayout = () => {
   return (
     <Container>
-      <div className="d-flex justify-content-between">
+      <nav className="d-flex justify-content-between">
         <Link to="/host/dashboard">Dashboard</Link>
         <Link to="/host/income">Income</Link>
         <Link to="/vans">Vans</Link>
         <Link to="/host/reviews">Reviews</Link>
-      </div>
+      </nav>
       <Outlet />
     </Container>
   );
