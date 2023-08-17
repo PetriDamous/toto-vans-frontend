7dc649872d4b67f4f47660802011c464026ec4a5
Dimitri Williams <dwilliams_64@yahoo.com>
2023-08-17 17:10:08 -0500

    Feat: Setup basic layout for vans page so I can practice using search params for filtering vans.

--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Container } from "react-bootstrap";
+import data from "../../data/data.json";
+
 const Vans = () => {
-  return <h1>Vans</h1>;
+  const [vans, setVans] = useState(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typeFilter = searchParams.get("type");
+
+  console.log(typeFilter);
+
+  useEffect(() => {
+    setVans(data);
+  }, []);
+
+  if (!vans) return "loading...";
+
+  return (
+    <Container>
+      <Row>
+        {vans.map((van) => (
+          <Col key={van.id} md={6}>
+            <Card style={{ width: "20rem" }}>
+              <Card.Img variant="top" src={`${van.imageUrl}`} />
+              <Card.Body>
+                <Card.Title>{van.name}</Card.Title>
+                <Card.Text>{van.description}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Vans;
