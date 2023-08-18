cded4238014660fd3f5b2d609864429de496cb35
Dimitri Williams <dwilliams_64@yahoo.com>
2023-08-18 18:55:20 -0500

    Feat: Filtered vans based on what search params are in the URL. Created Select component to hold radio buttons that allowed for search filtering based search params passed to URL from radio button.  All updates where made in Vans component.

--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
 import { Container } from "react-bootstrap";
 import data from "../../data/data.json";
 
@@ -14,18 +15,23 @@ const Vans = () => {
 
   const typeFilter = searchParams.get("type");
 
-  console.log(typeFilter);
-
   useEffect(() => {
     setVans(data);
   }, []);
 
   if (!vans) return "loading...";
 
+  const displayVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
+
   return (
     <Container>
+      <div className="mb-4">
+        <Select setSearchParams={setSearchParams} />
+      </div>
       <Row>
-        {vans.map((van) => (
+        {displayVans.map((van) => (
           <Col key={van.id} md={6}>
             <Card style={{ width: "20rem" }}>
               <Card.Img variant="top" src={`${van.imageUrl}`} />
@@ -39,6 +45,63 @@ const Vans = () => {
         ))}
       </Row>
     </Container>
+  );
+};
+
+const Select = ({ setSearchParams }) => {
+  const handleRadioChange = (e) => {
+    const typeValue = e.target.labels[0].title;
+
+    if (typeValue === "clear") {
+      setSearchParams({});
+      return;
+    }
+
+    setSearchParams({ type: typeValue });
+  };
+
+  const raidoCommonProps = {
+    inline: true,
+    type: "radio",
+    name: "vanFilter",
+    onChange: handleRadioChange,
+  };
+
+  const radioButtons = [
+    {
+      id: "simple-1",
+      title: "simple",
+      label: "Simple",
+      ...raidoCommonProps,
+    },
+    {
+      id: "luxury-1",
+      title: "luxury",
+      label: "Luxury",
+      ...raidoCommonProps,
+    },
+    {
+      id: "rugged-1",
+      title: "rugged",
+      label: "Rugged",
+      ...raidoCommonProps,
+    },
+    {
+      id: "clear-1",
+      title: "clear",
+      label: "Clear",
+      ...raidoCommonProps,
+    },
+  ];
+
+  return (
+    <Form>
+      {radioButtons.map((radioButton) => (
+        <div key={`inline-${radioButton.id}`} className="mb-3">
+          <Form.Check {...radioButton} />
+        </div>
+      ))}
+    </Form>
   );
 };
 
