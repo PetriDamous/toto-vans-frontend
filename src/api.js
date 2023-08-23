043dedfe385ed52e28ff6548c5a4b0c7fa2032ca
Dimitri Williams <dwilliams_64@yahoo.com>
2023-08-23 18:36:05 -0500

    Refact: Added id parameter to getVans function.  This way if an id is provided in the parameter a single van will be fetched from out json object. If id is not provided all vans in json object will be returned.

--- src/api.js
+++ src/api.js
@@ -1,21 +1,25 @@
 import data from "./data/data.json";
 
-export async function getVans() {
+export async function getVans(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const randomNumber = Math.random();
 
       const isError = randomNumber >= 0.5;
 
-      if (isError) {
-        return reject({
-          message: "Failed to fetch vans",
-          statusText: "Bad Request",
-          status: 400,
-        });
+      // if (isError) {
+      //   return reject({
+      //     message: "Failed to fetch vans",
+      //     statusText: "Bad Request",
+      //     status: 400,
+      //   });
+      // }
+
+      if (id) {
+        return resolve(data.find((van) => van.id === id));
       }
 
       return resolve(data);
-    }, 1000);
+    }, 500);
   });
 }
